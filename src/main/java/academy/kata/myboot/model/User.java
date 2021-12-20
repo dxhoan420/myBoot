1dafe6d009f2cae85b92c86d9c7621a0dbe296c2
Роман <Roks420@yandex.ru>
2021-12-20 19:49:56 +0300

    Added hasCode and equals methods based on value of email in User.java

--- src/main/java/academy/kata/myboot/model/User.java
+++ src/main/java/academy/kata/myboot/model/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Objects;
 
 @Entity @Table(name = "users")
 @Getter @Setter @ToString
@@ -33,6 +34,19 @@ public class User implements UserDetails {
             }
         }
         return resultString.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getEmail().equals(user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmail());
     }
 
     @Override
